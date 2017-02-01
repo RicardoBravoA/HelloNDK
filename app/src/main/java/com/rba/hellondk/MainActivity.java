a2727020165bd762e9b940e1bbc4f093c1d068a4
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-01 10:26:43 -0500

    chore(init): initial commit

---
+++ app/src/main/java/com/rba/hellondk/MainActivity.java
@@ -0,0 +1,36 @@
+package com.rba.hellondk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.lblDescription) AppCompatTextView lblDescription;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        lblDescription.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
